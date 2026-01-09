6371747009d894f6490a6bb866acc484b29dac68
lucianvariu <lucian.variu@stud.ubbcluj.ro>
2026-01-09 19:34:53 +0200

    scheletul proiectului

--- proiect.asm
+++ proiect.asm
@@ -1,7 +1,16 @@
 assume cs:code, ds:data, ss:stiva
 
+stiva segment stack
+    dw 256 dup(?)
+    top_stiva label word
+stiva ends
+
+
 data segment
-    
+    ;luci
+    ;ionut
+    ;rares
+    msg_test db 'Proiect initializat cu succes!', 13, 10, '$'
 data ends
 
 
@@ -12,7 +21,19 @@ start:
     mov ax, data
     mov ds, ax
     
-    ;testare branch
+    mov ax, stiva
+    mov ss, ax
+    lea sp, top_stiva
+
+    ; --- Test ca merge scheletul ---
+    mov ah, 09h
+    lea dx, msg_test
+    int 21h
+
+    ; call CITIRE_DATE      ; luci
+    ; call CALCUL_C         ; ionut
+    ; call SORTARE_AFISARE  ; rares
+    ; call ROTIRE_SIR       ; ionut
 
     mov ax, 4c00h
     int 21h
