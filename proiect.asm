1a875fb7b2f5ff362c5a41e3add7eacc18f18a8a
Rares536 <rteusdea7@gmail.com>
2026-01-11 14:41:40 +0200

    Am adaugat sortarea si afisarea finala

--- proiect.asm
+++ proiect.asm
@@ -17,7 +17,14 @@ data segment
 
     cuvant_C        dw 0    ; Variabila C (word 16 biti)
 
-    ;rares
+    msg_sort        db 13, 10, '>> Sirul sortat descrescator: $'
+    msg_max_b       db 13, 10, '>> Pozitie octet cu max biti 1 (>3): $'
+    msg_C_hex       db 13, 10, '>> Cuvantul C (Hex): $'
+    msg_C_bin       db 13, 10, '>> Cuvantul C (Bin): $'
+    msg_rot         db 13, 10, '>> Sirul dupa rotiri (Hex): $'
+    msg_rot_bin     db 13, 10, '>> Sirul dupa rotiri (Bin): $'
+    msg_sp          db ' $'
+    msg_none        db ' -$'
     
 data ends
 
@@ -36,8 +43,9 @@ start:
 
      call CITIRE_DATE      ; luci
      call CALCUL_C         ; ionut
-    ; call SORTARE_AFISARE  ; rares
+     call SORTARE_AFISARE  ; rares
      call ROTIRE_SIR       ; ionut
+     call AFISARE_FINAL_ROTIT  ; rares
 
     mov ax, 4c00h
     int 21h
@@ -223,6 +231,230 @@ NXT_R:
     loop ROT_L
     ret
 ROTIRE_SIR endp
+
+SORTARE_AFISARE proc near
+    ; 1. Afisare C (Calculat de Ionut anterior)
+    mov ah, 09h
+    lea dx, msg_C_hex
+    int 21h
+    
+    mov ax, cuvant_C
+    mov bh, ah
+    mov bl, al
+    mov al, bh
+    call PR_BYTE      ; Afisam partea de sus (High)
+    mov al, bl
+    call PR_BYTE      ; Afisam partea de jos (Low)
+    
+    mov ah, 09h
+    lea dx, msg_C_bin
+    int 21h
+    mov ax, cuvant_C
+    call PR_WORD_BIN  ; Afisam C in binar
+
+    ; 2. Sortare Bubble Sort Descrescator
+    cmp lungime_sir, 1
+    jbe GATA_SORT     ; Daca e 0 sau 1 element, nu sortam
+    
+    xor cx, cx
+    mov cl, lungime_sir
+    dec cl            ; Loop exterior (N-1)
+OUT_S:
+    push cx
+    lea si, sir_octeti
+    mov cl, lungime_sir
+    dec cl            ; Loop interior
+IN_S:
+    mov al, [si]
+    mov bl, [si+1]
+    cmp al, bl
+    jae NO_SW         ; Daca AL >= BL, nu schimba (vrem descrescator)
+    mov [si], bl      ; Swap
+    mov [si+1], al
+NO_SW:
+    inc si
+    dec cl
+    jnz IN_S
+    
+    pop cx
+    loop OUT_S
+
+GATA_SORT:
+    ; 3. Afisare Sir Sortat
+    mov ah, 09h
+    lea dx, msg_sort
+    int 21h
+    call PRINT_SIR
+    
+    ; 4. Gasire Max Biti (Statistica)
+    call GASIRE_MAX_BITI
+    ret
+SORTARE_AFISARE endp
+
+AFISARE_FINAL_ROTIT proc near
+    mov ah, 09h
+    lea dx, msg_rot
+    int 21h
+    call PRINT_SIR
+    
+    mov ah, 09h
+    lea dx, msg_rot_bin
+    int 21h
+    
+    ; Afisare sir binar extins
+    xor cx, cx
+    mov cl, lungime_sir
+    lea si, sir_octeti
+P_BIN_LOOP:
+    xor ax, ax
+    mov al, [si]
+    push cx
+    push ax
+    call PR_BYTE_BIN_ONLY 
+    mov ah, 02h
+    mov dl, ' '
+    int 21h
+    pop ax
+    pop cx
+    inc si
+    loop P_BIN_LOOP
+    ret
+AFISARE_FINAL_ROTIT endp
+
+GASIRE_MAX_BITI proc near
+    xor cx, cx
+    mov cl, lungime_sir
+    lea si, sir_octeti
+    mov bl, 0       ; Max curent
+    mov bh, 0       ; Pozitie (index)
+    mov dx, 0       ; Index curent (0, 1, 2...)
+SCAN:
+    mov al, [si]
+    push cx
+    mov cx, 8
+    xor ah, 0       ; Contor biti de 1 pentru numarul curent
+CNT:
+    shr al, 1
+    jnc NOT_1
+    inc ah
+NOT_1:
+    loop CNT
+    pop cx
+    
+    ; Verificam conditia > 3
+    cmp ah, 3
+    jbe SKIP_MAX    ; Daca are <= 3 biti, nu ne intereseaza
+    
+    cmp ah, bl
+    jbe SKIP_MAX    ; Daca nu e mai mare decat maximul gasit pana acum
+    mov bl, ah      ; Noul maxim
+    mov bh, dl      ; Noua pozitie
+SKIP_MAX:
+    inc si
+    inc dx          ; Crestem indexul
+    dec cl
+    jnz SCAN
+    
+    mov ah, 09h
+    lea dx, msg_max_b
+    int 21h
+    
+    cmp bl, 0       
+    je NO_RES
+    mov al, bh
+    inc al          ; Afisam pozitia 1-based (nu de la 0)
+    call PR_BYTE
+    ret
+NO_RES:
+    mov ah, 09h
+    lea dx, msg_none
+    int 21h
+    ret
+GASIRE_MAX_BITI endp
+
+; --- HELPERE AFISARE ---
+PRINT_SIR proc near
+    xor cx, cx
+    mov cl, lungime_sir
+    lea si, sir_octeti
+P_LOOP:
+    mov al, [si]
+    push cx
+    call PR_BYTE
+    mov ah, 02h
+    mov dl, ' '
+    int 21h
+    pop cx
+    inc si
+    loop P_LOOP
+    ret
+PRINT_SIR endp
+
+PR_BYTE proc near
+    push ax
+    push bx
+    push dx
+    mov bl, al
+    mov dl, al
+    shr dl, 4
+    call PR_DIG
+    mov dl, bl
+    and dl, 0Fh
+    call PR_DIG
+    pop dx
+    pop bx
+    pop ax
+    ret
+PR_BYTE endp
+
+PR_DIG proc near
+    cmp dl, 9
+    jbe IS_N
+    add dl, 7
+IS_N:
+    add dl, '0'
+    mov ah, 02h
+    int 21h
+    ret
+PR_DIG endp
+
+PR_WORD_BIN proc near
+    push cx
+    push bx
+    push dx
+    mov cx, 16
+    mov bx, ax
+WB_L:
+    shl bx, 1
+    mov dl, '0'
+    adc dl, 0
+    mov ah, 02h
+    int 21h
+    loop WB_L
+    pop dx
+    pop bx
+    pop cx
+    ret
+PR_WORD_BIN endp
+
+PR_BYTE_BIN_ONLY proc near
+    push cx
+    push bx
+    push dx
+    mov cx, 8
+    mov bl, al
+BB_L:
+    shl bl, 1
+    mov dl, '0'
+    adc dl, 0
+    mov ah, 02h
+    int 21h
+    loop BB_L
+    pop dx
+    pop bx
+    pop cx
+    ret
+PR_BYTE_BIN_ONLY endp
 
 
  
